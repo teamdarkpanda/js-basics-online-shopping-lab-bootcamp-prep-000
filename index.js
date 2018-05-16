773b7018fa4e92c3b8cb2dbef208955c553a7147
Ankur Puri <apuri@iit.edu>
2018-05-16 15:05:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -84,7 +84,7 @@ function placeOrder(cardNumber) {
   
   if(cardNumber !== undefined) {
     cart = []
-    return `Your total cost is $${cost}, which will be charged to the card ${cardNumber}`
+    return `Your total cost is $${cost}, which will be charged to the card ${cardNumber}.`
   } else {
     return "Sorry, we don't have a credit card on file for you."
   }
