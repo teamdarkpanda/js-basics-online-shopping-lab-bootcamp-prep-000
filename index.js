cd0acd818b217a5e928bfc2534a51aac6a64d923
Ankur Puri <apuri@iit.edu>
2018-05-16 15:02:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -80,7 +80,7 @@ function removeFromCart(item) {
 function placeOrder(cardNumber) {
   // write your code here
   
-  if(cardNumber !== undefined) {
+  if(`${cardNumber}` !== undefined) {
     
   } else {
     return "Sorry, we don't have a credit card on file for you."
